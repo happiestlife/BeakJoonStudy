fb3d196e70a3d4adb5b817613d44ca94f37ed084
chickenman <programmingStudy9999@gmail.com>
2021-08-16 18:48:48 +0900

    1935

---
+++ dataStructure/src/PostFix.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.Stack;
+public class PostFix {
+    static double calculate(double operand1, double operand2, char operation){
+        double result = 0;
+        switch(operation){
+            case '+':
+                result = operand1 + operand2;
+                break;
+            case '-':
+                result = operand1 - operand2;
+                break;
+            case '*':
+                result = operand1 * operand2;
+                break;
+            case '/':
+                result = operand1 / operand2;
+                break;
+        }
+        return result;
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Double> postfixStack = new Stack<>();
+
+        double result = 0;
+        int operandCount = Integer.parseInt(br.readLine());
+
+        String postfix = br.readLine();
+
+        double operandArray[] = new double[operandCount];
+        for(int i = 0; i < operandCount; i++)
+            operandArray[i] = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i<postfix.length(); i++){
+            char c = postfix.charAt(i);
+            if(c == '*' || c == '/' || c == '+' || c == '-'){
+                double secondOperand = postfixStack.pop();
+                double firstOperand = postfixStack.pop();
+                result = calculate(firstOperand, secondOperand, c);
+                postfixStack.push(result);
+            }else
+                postfixStack.push(operandArray[c-'A']);
+        }
+        System.out.printf("%.2f%n", result);
+    }
+}
