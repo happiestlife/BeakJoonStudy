03146d3b4fac8bc9945d1b9235ef2dffc2ee07d1
happiestlife <chickenman10@naver.com>
2022-01-14 01:39:29 +0900

    +40 subQuest success

--- dataStructure/src/Sliver/P17225.java
+++ dataStructure/src/Sliver/P17225.java
@@ -1,18 +1,28 @@
 package Sliver;
 
 import java.io.*;
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class P17225 {
-    private static class Order{
+    private static class Order implements Comparable<Order>{
         private String color;
         private int makeT;
 
         public Order(String color, int makeT) {
             this.color = color;
             this.makeT = makeT;
+        }
+
+        @Override
+        public int compareTo(Order o) {
+            if(makeT == o.makeT){
+                if(color.equals("B") && o.color.equals("R")) return -1;
+                else if (color.equals("R") && o.color.equals("B")) return 1;
+                else return 0;
+            }
+            return makeT - o.makeT;
         }
     }
 
@@ -26,59 +36,46 @@ public class P17225 {
         B = Integer.parseInt(stk.nextToken());
         N = Integer.parseInt(stk.nextToken());
 
-        PriorityQueue<Order> mheap = new PriorityQueue<>(new Comparator<Order>() {
-            @Override
-            public int compare(Order o1, Order o2) {
-                if( o1.makeT > o2.makeT ) return 1;
-                else if(o1.makeT  < o2.makeT ) return -1;
-                else {
-                    if(o1.color.equals("B")){
-                        if(o2.color.equals("B"))
-                            return 0;
-                        else return -1;
-                    }else{
-                        if(o2.color.equals("B"))
-                            return 1;
-                        else return 0;
-                    }
-                }
-            }
-        });
+        PriorityQueue<Order> mheap = new PriorityQueue<>();
 
-
-       for(int i = 0 ; i < N; i++){
+        int afinish = 0, bfinish = 0;
+        for(int i = 0 ; i < N; i++){
             String input[] = br.readLine().split(" ");;
 
             orderTime = Integer.parseInt(input[0]);
             color = input[1];
             num = Integer.parseInt(input[2]);
 
-            int makeT = (color.equals("B") ? A : B);
-            for(int j = 0; j < num; j++){
-                mheap.add(new Order(color, orderTime + j * makeT));
-            }
+            boolean isBlue = color.equals("B");
+            int makeT = (isBlue ? A : B);
+            int finishT = (isBlue ? afinish : bfinish);
+            int startT = (finishT < orderTime ? orderTime : finishT);
+            for(int j = 0; j < num; j++)
+                mheap.add(new Order(color, startT + j * makeT));
+
+            if(isBlue) afinish = startT + num * A;
+            else bfinish = startT + num * B;
         }
 
-        String sm = "";
-        String js = "";
+        int sm[] = new int[100000];
+        int js[] = new int[100000];
         int number = 1, smnum = 0, jsnum = 0;
         while (!mheap.isEmpty()) {
             Order o = mheap.poll();
-            if(o.color.equals("B")){
-                sm += number + " ";
-                smnum++;
-            }
-            else{
-                js += number + " ";
-                jsnum++;
-            }
+            
+            if(o.color.equals("B")) sm[smnum++] = number;
+            else js[jsnum++] = number;
+
             number++;
         }
 
         System.out.println(smnum);
-        System.out.println(sm);
+        for(int i = 0; i < smnum; i++)
+            System.out.print(sm[i]+" ");
+        System.out.println();
         System.out.println(jsnum);
-        System.out.println(js);
+        for(int i = 0; i < jsnum; i++)
+            System.out.print(js[i]+" ");
 
         br.close();
     }
